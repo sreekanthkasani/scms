57c41fdba902afad7c6ea6467cfb87130f9f0e5b
sreekanthkasani <sreekanthkasani22@gmail.com>
2017-11-23 11:00:08 +0530

    from other project

---
+++ newsDatastore.java
@@ -0,0 +1,39 @@
+public class newsDataStore {
+
+    private String name;
+    private String title;
+    private String url;
+    private String urlToImage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+}
