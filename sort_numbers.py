6251e35c663b09e6b3a03a1b94d366c7cb9fc057
sreekanth kasani <sreekanthkasani22@gmail.com>
2025-05-02 10:38:22 +0530

    Add simple Python script for sorting numbers

---
+++ sort_numbers.py
@@ -0,0 +1,7 @@
+def sort_numbers(numbers):
+    return sorted(numbers)
+
+if __name__ == '__main__':
+    numbers = [5, 2, 9, 1, 5, 6]
+    sorted_numbers = sort_numbers(numbers)
+    print('Sorted numbers:', sorted_numbers)
